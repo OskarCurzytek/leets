3ace8e8bc01d86739bca060596d777314ef94763
OskarCurzytek <oskarcurzytek@gmail.com>
2024-12-23 16:51:52 +0100

    27

---
+++ leet27/src/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean carry = false;
+        digits[digits.length - 1] += 1;
+
+        for(int i = digits.length - 1; i >= 0; i--){
+
+            if(carry){
+                digits[i] += 1;
+                carry = false;
+            }
+
+            if(digits[i] >= 10){
+                digits[i] -= 10;
+                carry = true;
+            }
+
+            if(i == 0 && carry){
+                int[] result = new int[digits.length+1];
+                result[0] = 1;
+                for(int j = 1; j < result.length - 1; j++){
+                    result[j] = digits[j-1];
+                }
+                return result;
+            }
+        }
+
+        return digits;
+    }
+}
