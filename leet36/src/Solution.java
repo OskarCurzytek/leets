786df5a119a2525fd7b2b4c21e5da6e3a8be40e7
OskarCurzytek <oskarcurzytek@gmail.com>
2025-01-12 14:12:35 +0100

    36

---
+++ leet36/src/Solution.java
@@ -0,0 +1,60 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        if(root == null){
+            return new ArrayList<>();
+        }
+
+        TreeNode current = root;
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(current);
+
+
+        while(!q.isEmpty()){
+
+            List<Integer> subResult = new ArrayList<>();
+            int size = q.size();
+
+            for(int i = 0; i < size; i++){
+
+                current = q.poll();
+                subResult.add(current.val);
+
+                if(current.left != null){
+                    q.add(current.left);
+                }
+
+                if(current.right != null){
+                    q.add(current.right);
+                }
+            }
+
+            result.add(subResult);
+        }
+
+        return result;
+    }
+}
